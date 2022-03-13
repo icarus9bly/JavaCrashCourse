a8ee855d4a2a5e1541d78f600bc5c544e05e6286
Aditya Singh <adips@amazon.com>
2022-03-13 13:46:05 +0530

    Initial commit

---
+++ src/com/aditya/Main.java
@@ -0,0 +1,10 @@
+package com.aditya;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String aa= "45890";
+        System.out.println(aa);
+    }
+}
---
+++ src/com/aditya/MyStuff.java
@@ -0,0 +1,7 @@
+package com.aditya;
+
+public class MyStuff {
+    public static void main (String[] args){
+        System.out.println("Bling Bling Bling");
+    }
+}
