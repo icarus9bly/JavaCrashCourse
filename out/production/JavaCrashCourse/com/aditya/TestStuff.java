8ddcde38fe5abd64b68ebbea7890fdce3d6c76c5
Aditya Singh <adips@amazon.com>
2022-03-21 00:21:42 +0530

    Add TestClass to experiment

---
+++ out/production/JavaCrashCourse/com/aditya/TestStuff.java
@@ -0,0 +1,21 @@
+package com.aditya;
+// import java.util.Scanner;
+/**
+ * Sample Main class.
+ */
+public class TestStuff {
+    /**
+     * Sample Main method.
+     * @return nothing.
+     */
+    public static void main(String[] args) {
+        // Code goes here
+        String aa = "45890";
+        System.out.println(aa.length());
+        System.out.println(aa.charAt(3));
+        System.out.print("Enter bro: ");
+        // Scanner kb = new Scanner(System.in);
+        // String out = kb.nextLine();
+        // System.out.println(out);
+    }
+}
