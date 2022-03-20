38cb654a62edf3ac053ee5eb2c03ff6006fc4e44
Aditya Singh <adips@amazon.com>
2022-03-21 00:13:41 +0530

    Initial commit

--- src/com/aditya/Main.java
+++ src/com/aditya/Main.java
@@ -1,10 +1,18 @@
 package com.aditya;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         String aa= "45890";
-        System.out.println(aa);
+        System.out.println(aa.length());
+        System.out.println(aa.charAt(3));
+        System.out.print("Enter bro: ");
+        Scanner kb= new Scanner(System.in);
+        String out=kb.nextLine();
+        System.out.println(out);
+
     }
 }
